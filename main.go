9d95402e8feef3baf14270bf809812bd62481dd7
reubendevries <reubendevries@gmail.com>
2021-09-22 09:00:42 -0700

    added MillisToReadable function

--- main.go
+++ main.go
@@ -9,4 +9,7 @@ func main() {
 
 	n := util.GetRandomInt()
 	fmt.Println(n)
+
+	h, m, s, ms := util.MillisToReadable()
+	fmt.Printf("Hour: %d, Minute: %d, Second: %d, Millisecond: %d\n", h, m, s, ms)
 }
--- util/util.go
+++ util/util.go
@@ -9,3 +9,12 @@ func GetRandomInt() int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Int()
 }
+
+func MillisToReadable() (int, int, int, int64) {
+	h := time.Now().Hour()
+	m := time.Now().Minute()
+	s := time.Now().Second()
+	ms := time.Now().UnixMilli()
+	return h, m, s, ms
+
+}
