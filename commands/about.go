aba9623a977c4c5bf32a78332789e2e719a78ff7
eq03642 <reuben.devries@bell.ca>
2021-10-04 10:54:27 -0700

    added struct for about

--- commands/about.go
+++ commands/about.go
@@ -1,1 +1,17 @@
 package commands
+
+import "time"
+
+type About struct {
+	Name             string
+	Description      string
+	Cooldown         int
+	Alias            []int
+	AdditionalFields struct {
+		Author  string
+		Version int
+		License string
+		Uptime  time.Duration
+		Name    string
+	}
+}
