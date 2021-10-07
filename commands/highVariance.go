8a74163faee5d0eeb09303665d3fc02f53616d0c
reubendevries <reubendevries@gmail.com>
2021-10-06 19:47:53 -0700

    added highVariance regular go files

--- commands/highVariance.go
+++ commands/highVariance.go
@@ -1,1 +1,36 @@
 package commands
+
+import (
+	"math"
+	"math/rand"
+)
+
+func highVariancePercent() {
+	tens := []int{
+		00,
+		00,
+		10,
+		10,
+		20,
+		70,
+		80,
+		80,
+		90,
+		90,
+	}
+	ones := []int{
+		1,
+		1,
+		2,
+		2,
+		3,
+		8,
+		9,
+		9,
+		10,
+		10,
+	}
+	result := tens{math.Floor(rand.Int() * len(tens))}
+	result += ones{math.Floor(rand.Int() * len(ones))}
+	return result
+}
--- commands/regular.go
+++ commands/regular.go
@@ -1,1 +1,43 @@
 package commands
+/*
+const Discord = require('discord.js');
+
+const {embedColor} = require('../config.json');
+
+const {getRandomInt,rollOutput} = require('../util.js');
+
+module.exports = {
+  name: 'regular',
+  description: 'Roll a regular die',
+  aliases: ['d'],
+  execute(message, args) {
+    var rollResults = []
+    for (r = args.rolls; r > 0; r-- ) {
+      rollResults.push(getRandomInt(1, args.sides));
+    }
+
+    result = rollOutput(resultList=rollResults, modifier=args.plusMinus)
+
+    const resultEmbed = new Discord.MessageEmbed()
+      .setColor(embedColor)
+
+    if ( !result.total ) {
+      resultEmbed
+        .setTitle('Sorry!')
+        .setDescription('Invalid command syntax')
+    }
+    else {
+      resultEmbed
+        .setTitle(result.total)
+        .setFooter(result.formula || '')
+    }
+
+    message.reply(resultEmbed);
+  }
+};
+*/
+
+func execute() {
+	var rollResults []int
+	for i := 0
+}
